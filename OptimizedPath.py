f7c633055c357016f8af9a7969d9fd28e6268610
zwede <zwedemeyers@gmail.com>
2023-05-17 12:43:02 -0700

    minoir changes

--- OptimizedPath.py
+++ OptimizedPath.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 plotterSize = [1979, 1000] # [x size, y size]
 
+
 # implementation of traveling Salesman Problem
 def travellingSalesmanProblem(D):
     # D : distance matrix. the first row will be treated as the starting node
@@ -22,7 +23,6 @@ def travellingSalesmanProblem(D):
     visited[next] = True # says the first node has already been visited
     for ii in range(l[0]):
         D[next,visited] = maxDist + 1 # add current node to the exclusion list
-        print(np.amin([D[next,:]]))
         idx = np.where(np.amin(D[next,:]) == D[next,:]) # find the closest node
         next = idx[0][0] # set that as the next node
         minPath[ii] = next # record that thats the next node
@@ -35,6 +35,11 @@ def travellingSalesmanProblem(D):
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename(filetypes=[("DXF files", "*.dxf")])
+
+def make_output_file():
+    saveFileName = file_path.split("/")[-1].split(".")[0]
+    saveFileName += ".gcode"
+    return saveFileName
 
 try:
     doc, auditor = recover.readfile(file_path)
@@ -71,15 +76,13 @@ for entity in msp:
 
         prevEnd = entity.dxf.end # save end point of the last line for next loop itteration
         ii = ii + 1 # itterate counter
+
 pts = pts[0:pts_count,:] # get rid of extra entries
 pts[0,:] = [0,plotterSize[1]] # set starting point
-
 
 # Calculates the Distance matrix from set of points in pts
 D = np.sqrt(np.sum((pts[None, :] - pts[:, None])**2, -1))
 minPath = travellingSalesmanProblem(D) # find min path
-print(minPath)
-
 
 # function to show the plot
 for c in L:
@@ -88,6 +91,54 @@ for c in L:
 pts = pts[minPath,:]
 plt.plot(pts[:,0] , pts[:,1])
 plt.show()
+print(pts)
+print(minPath)
+print(len(minPath))
 
+# generate gcode
+# definitions
+def liftPen(f):f.write(f'G4 P{penDelay}\nM280 P0 S100 G0 Z10 \n')
+def lowerPen(f):f.write(f'G4 P{penDelay}\nM280 P0 S0 G0 Z0\n')
+def homePen(f):f.write(f'G28\n')
 
-       
+# travels without drawing
+def travelMove(x,y):
+    f.write(f'; TravelMove\n')
+    f.write(f'M204 T{travelAccel}\n') # change to travel acceleration
+    liftPen(f) # pen up
+    f.write(f'G0 X{x:.3f} Y{y:.3f}\n') # go to final position
+    f.write(f'M204 T{drawAccel}\n') # change back to draw acceleration
+    lowerPen(f) # pen down
+
+def drawMove(x,y,xprev):
+    # continue to next postiion
+    f.write(f'G0 X{x:.3f} Y{y:.3f}\n')
+
+with open(make_output_file(), 'w') as f:
+    
+    # claer file
+    f.truncate(0)
+
+    # definitions
+    x_0 = 0
+    y_0 = 0 
+
+    # G-code HEADER    
+    f.write(f'; HEADER\n')
+    f.write(f'G0 F{FeedRate}\n') # set feedrate
+    f.write(f'M205 X{jerk}\n') # set x jerk
+    liftPen(f)
+    homePen(f)
+
+    # G-code BODY
+    f.write(f'; Body\n')
+    # iterate through every entitiy in the DXF file
+    #for i in range(len(L)):
+    #    for xy in L[minPath]
+
+    # G-code FOOTER
+    f.write(f'; FOOTER\n')
+    liftPen(f)
+    f.write(f'G0 X0\n') # Gets Y axis out of the way
+    f.write(f'M84\n') # Disable Steppers
+    f.write(f'M282\n') # dePowers Servo
