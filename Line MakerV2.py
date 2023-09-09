5ecc40fff6763f32532e39dfb69e7badf8776db4
zwede <zwedemeyers@gmail.com>
2023-09-09 13:02:11 -0700

    tada

--- Line MakerV2.py
+++ Line MakerV2.py
@@ -4,14 +4,15 @@ import numpy as np
 import cv2
 from g_encoder import GEncoder
 
-printArea = (800, 300) # (Y, X) in mm
+printArea = (250, 200) # (Y, X) in mm
 
-lineDist = 7 # distance between lines in mm
-lineHeight = 7 # line height in mm
+lineDist = 2 # distance between lines in mm
+lineHeight = 2 # line height in mm
 
-blurLen = 4
+
+blurLen = 2
 start = 0
-img = cv2.imread('test2.jpg')
+img = cv2.imread('test1.jpg')
 
 # Resize image to print volume size wtihout distorting
 scaleY = (printArea[0] / img.shape[0])
@@ -33,34 +34,33 @@ else:
     A = np.vstack((filler,A,filler))
 
 print(A.shape)
+A = A/-255 + 1 # Invert and normalize between 0 and 1
 # blur image
 filt = np.ones(blurLen)/blurLen
 # Blur image row by row
-for n in range(blurLen):
+for n in range(3):
     for Y in range(0,A.shape[0]):
         row = np.convolve(A[Y,:],filt,mode='same')
         A[Y,:] = row
 
 
-
-# make a new array to become the line image
-L = np.ones(A.shape)*255
-
-
 # loop though and add lines
 all = []
+lineNum = 0
 for Y in range(lineDist-start, round((A.shape[0])-1)-start, lineDist):
+    lineNum = lineNum + 1
     line = []
-    prevShift = lineHeight
-    if (Y % 2) == 0:
+    prevShift = 0
+    if (lineNum % 2) == 0:
         iterat = range(0,A.shape[1])
     else:
         iterat = range(A.shape[1]-1,-1,-1)
 
     for X in iterat:
-        shift = ((A[Y,X] / -255)+1)
-        if prevShift < lineHeight/255
-            line.append((X,Y + shift*lineHeight))
+        shift = (A[Y,X])
+        if (abs(prevShift-shift) > lineHeight/255) or X==A.shape[1]-1 or X==0:
+            firstTime = 0
+            line.append((X,(1000 - printArea[0]) + Y + shift*lineHeight))
         prevShift = shift
         
     all.append(line)
@@ -72,11 +72,7 @@ with open('lines.gcode', "w") as f:
 
 A = np.around(((A-np.amin(A))/np.amax(A))*255) # normalize between 0 and 255
 A = A.astype(np.uint8) # cast into proper data type
-
-
-
-L = L.astype(np.uint8) # case into proper data type
-output = np.dstack((L,L,L))
+output = np.dstack((A,A,A))
 # save
 data = im.fromarray(output) # creating image object of array
 data.save('testProcessed.png') # saving the final output 
